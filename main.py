22abe485bf43d81eac234652b97a6f14fa908476
anderson <andersonrochatavares@gmail.com>
2020-04-19 23:26:34 -0300

    working prototype

---
+++ main.py
@@ -0,0 +1,62 @@
+import fire
+import dataset
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, precision_score
+
+
+def main(cnf='instances/bw_large.d.cnf'):
+    """
+    Runs the prototype, executing the following steps:
+
+    Receives a boolean formula specified in CNF format,
+    Generates many sat and unsat samples for the formula,
+    Trains a classifier on this dataset,
+    Outputs performance metrics.
+
+    :param cnf:
+    :return:
+    """
+    data_x, data_y = dataset.generate_dataset(cnf)
+    learner = DecisionTreeClassifier()
+    run_model(learner, data_x, data_y)
+
+
+def run_model(model, data_x, data_y, splitter=StratifiedKFold(n_splits=5)):
+    """
+    Runs a machine learning model in the specified dataset.
+    For each train/test split on the dataset, it outputs the number
+    of samples, precision and accuracy to the standard output.
+
+    :param model: a learning algorithm that implements fit and predict methods
+    :param data_x: instances of the dataset
+    :param data_y: labels of the dataset
+    :param splitter: an object that implements split to partition the dataset (useful for cross-validation, for example)
+    :return:
+    """
+
+    # prints the header
+    print('#instances\tprec\tacc')
+
+    # trains the model for each split (fold)
+    for train_index, test_index in splitter.split(data_x, data_y):
+        # splits the data frames in test and train
+        train_x, train_y = data_x.iloc[train_index], data_y.iloc[train_index]
+        test_x, test_y = data_x.iloc[test_index], data_y.iloc[test_index]
+
+        model.fit(train_x, train_y)
+
+        # obtains the predictions on the test set
+        predictions = model.predict(test_x)
+
+        # calculates and reports some metrics
+        acc = accuracy_score(predictions, test_y)
+        prec = precision_score(predictions, test_y, average='macro')
+        print('{}\t\t{:.3f}\t{:.3f}'.format(len(test_y), prec, acc))
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
+    print("Done")
