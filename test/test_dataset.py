74f4b156d73641d8bafb6ba9ec5e6353b8034ca2
anderson <andersonrochatavares@gmail.com>
2020-06-04 16:14:20 -0300

    generate_dataset has been tested, all green

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import os
 from pysat.formula import CNF
 import unittest
 
@@ -106,6 +106,138 @@ class TestDataset(unittest.TestCase):
         retrieved_set = set([tuple([lit for lit in pos]) for pos in retrieved])
 
         self.assertEqual(expected_positives, retrieved_set)
+
+    def test_unigen_generate_dataset_small_formula(self):
+
+        f = CNF(from_clauses=[[1, 2, 3], [4]])
+        '''
+        the formula above has 7 positive samples:
+        positives = {  
+            (1, 2, 3, 4),
+            (1, 2, -3, 4),
+            (1, -2, 3, 4),
+            (1, -2, -3, 4),
+            (-1, 2, 3, 4),
+            (-1, 2, -3, 4),
+            (-1, -2, 3, 4),
+        }
+        '''
+        f.to_file('/tmp/test_small.cnf')
+
+        sampler = dataset.UnigenDatasetGenerator()
+
+        # a formula with very few solutions will return an empty dataset
+        data_x, data_y = sampler.generate_dataset('/tmp/test_small.cnf', 50)
+        self.assertEqual(0, len(data_x))
+        self.assertEqual(0, len(data_y))
+
+        # deletes the temp file used to store the formula
+        os.unlink('/tmp/test_small.cnf')
+
+    def test_unigen_generate_dataset(self):
+        f = CNF(from_clauses=[[1, 2, 3, 4], [5, 6]])
+        # the formula above has 45 positive & 19 negative samples
+        # positives enumerated below (with Glucose3)
+        positives = {
+            (1, -2, -3, -4, 5, -6),
+            (1, 2, -3, -4, 5, -6),
+            (1, 2, 3, -4, 5, -6),
+            (1, 2, 3, 4, 5, -6),
+            (1, 2, 3, 4, 5, 6),
+            (-1, 2, 3, 4, 5, 6),
+            (-1, -2, 3, 4, 5, 6),
+            (-1, -2, -3, 4, 5, 6),
+            (-1, -2, 3, -4, 5, 6),
+            (-1, -2, 3, -4, -5, 6),
+            (-1, -2, 3, -4, 5, -6),
+            (1, -2, 3, -4, 5, -6),
+            (-1, 2, 3, -4, 5, -6),
+            (-1, 2, -3, -4, 5, -6),
+            (-1, 2, -3, 4, 5, -6),
+            (-1, 2, -3, 4, 5, 6),
+            (-1, 2, -3, -4, 5, 6),
+            (1, 2, -3, -4, 5, 6),
+            (1, -2, -3, -4, 5, 6),
+            (1, -2, 3, -4, 5, 6),
+            (1, -2, 3, -4, -5, 6),
+            (1, -2, 3, 4, 5, 6),
+            (1, -2, 3, 4, -5, 6),
+            (1, -2, -3, 4, 5, 6),
+            (1, -2, -3, 4, -5, 6),
+            (1, -2, -3, -4, -5, 6),
+            (1, 2, -3, -4, -5, 6),
+            (1, 2, 3, -4, 5, 6),
+            (1, 2, 3, -4, -5, 6),
+            (1, 2, 3, 4, -5, 6),
+            (1, 2, -3, 4, 5, 6),
+            (1, 2, -3, 4, -5, 6),
+            (-1, 2, -3, 4, -5, 6),
+            (-1, 2, -3, -4, -5, 6),
+            (-1, 2, 3, -4, 5, 6),
+            (-1, 2, 3, -4, -5, 6),
+            (-1, 2, 3, 4, -5, 6),
+            (-1, -2, 3, 4, -5, 6),
+            (-1, -2, -3, 4, -5, 6),
+            (1, 2, -3, 4, 5, -6),
+            (1, -2, -3, 4, 5, -6),
+            (-1, -2, -3, 4, 5, -6),
+            (-1, -2, 3, 4, 5, -6),
+            (-1, 2, 3, 4, 5, -6),
+            (1, -2, 3, 4, 5, -6)
+        }
+
+        # negatives enumerated below (by hand)
+        negatives = {
+            (-1, -2, -3, -4, -5, -6),  # negate first clause
+            (-1, -2, -3, -4, -5, 6),
+            (-1, -2, -3, -4, 5, -6),
+            (-1, -2, -3, -4, 5, 6),
+
+            (-1, -2, -3, 4, -5, -6),  # negate 2nd clause, 4th lit up
+            (-1, -2, 3, -4, -5, -6),  # negate 2nd clause, 3rd lit up
+            (-1, -2, 3, 4, -5, -6),   # ...
+
+            (-1, 2, -3, -4, -5, -6),  # negate 2nd clause, 2nd lit up
+            (-1, 2, -3, 4, -5, -6),  # ...
+            (-1, 2, 3, -4, -5, -6),  # ...
+            (-1, 2, 3, 4, -5, -6),  # ...
+
+            (1, -2, -3, -4, -5, -6),
+            (1, -2, -3, 4, -5, -6),  # negate 2nd clause, 1st lit up
+            (1, -2, 3, -4, -5, -6),
+            (1, -2, 3, 4, -5, -6),
+            (1, 2, -3, -4, -5, -6),
+            (1, 2, -3, 4, -5, -6),
+            (1, 2, 3, -4, -5, -6),
+            (1, 2, 3, 4, -5, -6),
+        }
+        f.to_file('/tmp/test.cnf')
+
+        sampler = dataset.UnigenDatasetGenerator()
+        data_x, data_y = sampler.generate_dataset('/tmp/test.cnf', 500)
+
+        # I expect the dataset to contain all 64 possible assignments
+        self.assertEqual(64, len(data_x))
+        self.assertEqual(64, len(data_y))
+
+        # create a new dataset with data_x and y appended column-wise
+        df = data_x.copy()
+        df['y'] = data_y
+
+        # convert expected to 0/1 instead of n/-n
+        binary_pos = {  # +[1]  because each one is a positive example
+            tuple([0 if lit < 0 else 1 for lit in assignment] + [1]) for assignment in positives
+        }
+        binary_neg = {  # +[0]  because each one is a positive example
+            tuple([0 if lit < 0 else 1 for lit in assignment] + [0]) for assignment in negatives
+        }
+
+        # creates a set out of the dataset and compare it with the expected one
+        dataset_set = set([tuple(row) for row in df.values])
+        self.assertEqual(binary_pos.union(binary_neg), dataset_set)
+
+        # deletes the temp file used to store the formula
+        os.unlink('/tmp/test.cnf')
 
 
 if __name__ == '__main__':
