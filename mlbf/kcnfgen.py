72126121fb14677cea5c413b13173817c0969dab
anderson <andersonrochatavares@gmail.com>
2020-08-21 18:22:06 -0300

    initial plot of instance generator

---
+++ mlbf/kcnfgen.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+
+import fire
+
+
+def kcnfgen(output, n, m, k=3):
+    """
+    Generates a random k-CNF with the specified parameters.
+    This is just a wrapper on cnfgen randkcnf passing the proper parameters.
+    :param output: prefix of where the files will be saved
+    :param n: number of variables
+    :param m: number of clauses
+    :param k: number of literals per clause
+    """
+    #for i in range(num_formulas):
+        # output = f'{dest_prefix}/{i}_{k}_{n}_{m}.cnf'
+    subprocess.call(['cnfgen', '--output', output, 'randkcnf', str(k), str(n), str(m)])
+
+
+def generate_instances(prefix, n, m, k=3, num_instances=100, tmpfile='/tmp/candidate.cnf'):
+    num_sat, num_unsat = 0, 0
+    for i in range(num_instances):
+        print('calling cnfgen')
+        kcnfgen(tmpfile, n, m, k)
+
+        # TODO collect statistics
+
+        try:  # checks satisfiability with minisat
+            print('calling minisat')
+            subprocess.check_call(['minisat', tmpfile], stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as ex:
+            if ex.returncode == 10:  # satisfiable
+                os.rename(tmpfile, f'{prefix}/sat_{num_sat:05}_{k}_{n}_{m}.cnf')
+                num_sat += 1
+            else:  # unsatisfiable
+                os.rename(tmpfile, f'{prefix}/sat_{num_unsat:05}_{k}_{n}_{m}.cnf')
+                num_unsat += 1
+
+
+if __name__ == '__main__':
+    fire.Fire()
