9fe3de5566df9c1227261cc13090261b8b9d4e44
anderson <andersonrochatavares@gmail.com>
2021-01-15 18:52:37 -0300

    allowing specification of formula sizes from commmand line

--- mlbf/sample_small.py
+++ mlbf/sample_small.py
@@ -5,15 +5,18 @@ import dataset
 import fire
 
 
-def sample_small(basedir, solver='Glucose3'):
+def sample_small(basedir, solver='Glucose3', *var_sizes):
     """
     Traverses the basedir, looking for instances without their corresponding dataset.
     Generates the dataset for these, using pysat-based sampler
-    :param basedir:
-    :param solver:
+    :param basedir: directory where the 'phase' dataset is located
+    :param solver: sat solver used to enumerate instances
+    :param var_sizes: will look for formuals with the number of variables in this list
     :return:
     """
-    var_sizes = range(10, 101, 10)  # [10,20,...,100]
+    if len(var_sizes) == 0:
+        var_sizes = range(10, 101, 10)
+
     for v in var_sizes:
         for cnf in glob.glob(f'{basedir}/v{v}/*/sat*.cnf'):
             if len(glob.glob(f'{cnf}*.pkl.gz')) == 0:
@@ -23,3 +26,6 @@ def sample_small(basedir, solver='Glucose3'):
 
 if __name__ == '__main__':
     fire.Fire(sample_small)
+
+# usage example:
+# python mlbf/sample_small.py instances/phase Glucose3 10 20 30
