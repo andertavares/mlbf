4c2758760bd0d9523f75acfb46379aa453b0ca5d
anderson <andersonrochatavares@gmail.com>
2021-01-16 22:11:18 -0300

    allowing specification of the number of positive & negative samples

--- mlbf/sample_small.py
+++ mlbf/sample_small.py
@@ -5,11 +5,13 @@ import dataset
 import fire
 
 
-def sample_small(basedir, solver='Glucose3', *var_sizes):
+def sample_small(basedir, positives, negatives, solver='Glucose3', *var_sizes):
     """
     Traverses the basedir, looking for instances without their corresponding dataset.
     Generates the dataset for these, using pysat-based sampler
     :param basedir: directory where the 'phase' dataset is located
+    :param positives: number of positive samples to generate
+    :param negatives: number of negative samples to generate
     :param solver: sat solver used to enumerate instances
     :param var_sizes: will look for formuals with the number of variables in this list
     :return:
@@ -21,11 +23,11 @@ def sample_small(basedir, solver='Glucose3', *var_sizes):
         for cnf in glob.glob(f'{basedir}/v{v}/*/sat*.cnf'):
             if len(glob.glob(f'{cnf}*.pkl.gz')) == 0:
                 print(f'Sampling for {cnf}')
-                dataset.generate_dataset(cnf, solver)
+                dataset.generate_dataset(cnf, solver, num_positives=positives, num_negatives=negatives)
 
 
 if __name__ == '__main__':
     fire.Fire(sample_small)
 
 # usage example:
-# python mlbf/sample_small.py instances/phase Glucose3 10 20 30
+# python mlbf/sample_small.py instances/phase --positives 5000 --negatives 5000 --solver Glucose3 10 20 30
